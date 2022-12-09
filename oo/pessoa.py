4bd3fd9208f854774e2e675fc94270f60bd9017a
Ruan Wendel <ruanwendel.rw@gmail.com>
2022-12-08 21:31:58 -0300

    Criados atribudos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=0):
+        self.nome = nome
+        self.idade = idade
     def cumprimentar(self):
         return 'Olá'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Wendel')
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Ruan'
+    print(p.nome)
