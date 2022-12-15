1696ebafb4b2006289ccf6692254cdefa9511fa8
Ruan Wendel <ruanwendel.rw@gmail.com>
2022-12-14 21:03:58 -0300

    Criado atributo de classe.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=0):
         self.nome = nome
         self.idade = idade
@@ -16,5 +18,12 @@ if __name__ == '__main__':
         print(filho.nome)
     wendel.sobrenome = 'Borges'
     del wendel.filhos
+    wendel.olhos = 1
+    del wendel.olhos
     print(wendel.__dict__)
     print(ruan.__dict__)
+    Pessoa.olhos = 2
+    print(Pessoa.olhos)
+    print(ruan.olhos)
+    print(wendel.olhos)
+    print(id(Pessoa.olhos), id(ruan.olhos), id(wendel.olhos))
